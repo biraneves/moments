908e316ac299b995d3d71d25aa948b2bcdc15c0e
Ubirajara Neves <biraneves@outlook.com>
2024-02-19 16:28:43 -0300

    Header component styling

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bn-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.scss']
+})
+export class NewMomentComponent {
+
+}
