9287cbec4068d6284de539193ca68bec5fb80e7d
Ubirajara Neves <biraneves@outlook.com>
2024-02-20 12:01:37 -0300

    Retrieve moments from db

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MomentsService } from 'src/app/services/moments.service';
+import { Moment } from 'src/app/interfaces/Moment';
+import { environment } from 'src/app/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'bn-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl: string = environment.baseApiUrl;
+
+  // TODO: Search
+
+  constructor(private momentService: MomentsService) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
+}
--- src/app/services/moments.service.ts
+++ src/app/services/moments.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
+import { Response } from '../interfaces/Response';
+import { Moment } from '../interfaces/Moment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +13,10 @@ export class MomentsService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
