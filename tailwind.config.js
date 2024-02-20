39d430fdabfd1309f06fc611ad847e2270711093
Ubirajara Neves <biraneves@outlook.com>
2024-02-20 18:21:20 -0300

    Better layout for the galley

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
-    colors: {
-      amber: "#fff8e7",
-      orange: "#fdba74",
-      blue: "#a3dff7",
-      green: "#a9d89f",
-      "gray-dark": "#484848",
-    },
     fontFamily: {
       sans: "Quicksand, sans-serif",
       display: "Montserrat, sans-serif",
